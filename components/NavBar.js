77575396b5df2e88cb0f949e51e53fa562fec255
root <root@localhost.localdomain>
2020-03-09 21:14:23 +0000

    update

---
+++ components/NavBar.js
@@ -0,0 +1,10 @@
+import Link from './Link'
+
+export default () => {
+    return (    
+        <>   
+            <Link href="/">Home</Link>
+            <Link href="/contact">Contact</Link>
+        </>
+    )
+}
