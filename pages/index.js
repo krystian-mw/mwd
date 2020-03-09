d8dde27f8778cf07258d02441ab30854b2e8a3a6
root <root@localhost.localdomain>
2020-03-09 21:19:08 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ const Home = () => {
         <>
             <Head>
                 <title>MWD - Home</title>
+                <meta name="description" content="My new fantastic webpage which is still under construction. Check it out soon!" />
             </Head>
             <h1>MWD</h1>
         </>
