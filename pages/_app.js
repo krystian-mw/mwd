05a9c74465c073a0ecf29f1031837ee78d5647da
krystian-mw <49584311+krystian-mw@users.noreply.github.com>
2020-03-09 20:28:07 +0000

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import App from 'next/app'
+import Link from 'next/link'
+import Router from 'next/router'
+import Head from 'next/head'
+
+import NProgress from 'nprogress'
+import { Container, Row } from 'react-bootstrap'
+
+import NavBar from '../components/NavBar'
+
+Router.events.on('routeChangeStart', url => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+          <link rel="stylesheet" type="text/css" href="/bootstrap.css" />
+        </Head>
+        <Container>
+            <Row>
+                <NavBar />
+            </Row>
+            <Row>
+                <Component {...pageProps} />
+            </Row>
+        </Container>
+      </>
+    )
+  }
+}
